325a3e12db481931dd510cfd36cb68b27fd64a6b
gus <gustavo.graz@outlook.com>
2020-05-09 11:15:55 +0200

    Add sidebar in each screen + specific tags for each sidebar

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -3,17 +3,11 @@ local gears = require('gears')
 local icons = require('theme.icons')
 local apps = require('configuration.apps')
 
-local tags = {
+local left = {
   {
     icon = icons.chrome,
     type = 'chrome',
     defaultApp = apps.default.browser,
-    screen = 1
-  },
-  {
-    icon = icons.code,
-    type = 'code',
-    defaultApp = apps.default.editor,
     screen = 1
   },
   {
@@ -23,9 +17,9 @@ local tags = {
     screen = 1
   },
   {
-    icon = icons.game,
-    type = 'game',
-    defaultApp = apps.default.game,
+    icon = icons.music,
+    type = 'music',
+    defaultApp = apps.default.music,
     screen = 1
   },
   {
@@ -34,17 +28,20 @@ local tags = {
     defaultApp = apps.default.files,
     screen = 1
   },
+}
+
+local right = {
   {
-    icon = icons.music,
-    type = 'music',
-    defaultApp = apps.default.music,
-    screen = 1
+    icon = icons.code,
+    type = 'code',
+    defaultApp = apps.default.editor,
+    screen = 2
   },
   {
     icon = icons.lab,
     type = 'any',
     defaultApp = apps.default.rofi,
-    screen = 1
+    screen = 2
   }
 }
 
@@ -54,25 +51,49 @@ awful.layout.layouts = {
   awful.layout.suit.floating
 }
 
-awful.screen.connect_for_each_screen(
-  function(s)
-    for i, tag in pairs(tags) do
-      awful.tag.add(
-        i,
-        {
-          icon = tag.icon,
-          icon_only = true,
-          layout = awful.layout.suit.tile,
-          gap_single_client = false,
-          gap = 4,
-          screen = s,
-          defaultApp = tag.defaultApp,
-          selected = i == 1
-        }
-      )
+for i, tag in pairs(left) do
+  awful.screen.connect_for_each_screen (
+    function(s)
+      if (s.index == tag.screen) then
+        awful.tag.add(
+          i,
+          {
+            icon = tag.icon,
+            icon_only = true,
+            layout = awful.layout.suit.tile,
+            gap_single_client = false,
+            gap = 4,
+            screen = s,
+            defaultApp = tag.defaultApp,
+            selected = i == 1
+          }
+        )
+      end
     end
-  end
-)
+  )
+end
+
+for i, tag in pairs(right) do
+  awful.screen.connect_for_each_screen (
+    function(s)
+      if (s.index == tag.screen) then
+        awful.tag.add(
+          i,
+          {
+            icon = tag.icon,
+            icon_only = true,
+            layout = awful.layout.suit.tile,
+            gap_single_client = false,
+            gap = 4,
+            screen = s,
+            defaultApp = tag.defaultApp,
+            selected = i == 1
+          }
+        )
+      end
+    end
+  )
+end
 
 _G.tag.connect_signal(
   'property::layout',
