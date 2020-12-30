de21d762bf5d43a2333d3df5254e9465f2425d8c
gus <gustavo.graz@outlook.com>
2020-12-30 16:52:14 +0100

    Remove franz + add telegram/whatsapp
    Signed-off-by: gus <gustavo.graz@outlook.com>

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -24,7 +24,7 @@ return {
     -- associated with each tag/workspace
     browser = 'chromium',
     editor = 'code',
-    social = { 'franz' },
+    social = { 'telegram-desktop', 'whatsapp-web' },
     mail = 'thunderbird',
     -- game = rofi_command,
     files = 'nautilus',
